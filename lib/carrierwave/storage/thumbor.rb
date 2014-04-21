49a50844bfd2e4d4fb472f7d40ac6b56a6f5f9a8
Mikhail Dronov <dronovmm@undev.ru>
2014-04-21 16:27:38 +0400

    added class Connection

--- lib/carrierwave/storage/thumbor.rb
+++ lib/carrierwave/storage/thumbor.rb
@@ -1,42 +1,61 @@
 module CarrierWave
   module Storage
     class Thumbor < Abstract
-      class File
-        attr_reader :thumbor_url, :location
+      class Connection
+        attr_reader :thumbor_url
 
-        def initialize(thumbor_url, location = nil)
-          @thumbor_url = thumbor_url
+        def initialize(options = {})
+          @thumbor_url = options[:thumbor_url]
+        end
+
+        def store(file)
+          response = RestClient.post "#{thumbor_url}/image", media: file
+          location = response.headers[:location]
+          identifier_from_location location
+        end
+
+        private
+
+        def identifier_from_location(location)
+          location[/\A\/image\/(.*)/, 1]
+        end
+      end
+
+      class File
+        attr_reader :uploader, :location
+
+        def initialize(uploader, location = nil)
+          @uploader = uploader
           @location = location
         end
 
         def url
-          "#{thumbor_url}/#{location}"
+          "#{@thumbor_url}/#{location}"
         end
 
         def store!(file)
-          #TODO: fix me
-          response = RestClient.post "#{thumbor_url}/image", media: file
-          @location = response.headers[:location].gsub /\/image\//, ''
-          p @location
+         connection.store file
         end
+
+        private
+
+        def connection
+          @connection ||= Connection.new thumbor_url: uploader.thumbor_url
+        end
+
       end
 
-      def identifier
-        p "="*20
-        p @identifier
-        @identifier
-      end
+      attr_reader :identifier
 
       def store!(file)
-        f = File.new uploader.thumbor_url
-        f.store!(::File.open(file.file))
-        @identifier = f.location
-        p @identifier
+        reopened_file = ::File.open(file.file)
+        f = File.new uploader
+        @identifier = f.store!(reopened_file)
         f
       end
 
       def retrieve!(identifier)
-        File.new uploader.thumbor_url, identifier
+        File.new uploader, identifier
       end
     end
   end
